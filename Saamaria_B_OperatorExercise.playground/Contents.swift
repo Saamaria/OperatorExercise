6ad0809ec07dbce9a8e26bc773af40a4d2229fe6
Saamaria <67989662+Saamaria@users.noreply.github.com>
2020-07-15 20:20:02 -0500

    Add files via upload

---
+++ Saamaria_B_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+// setup the timer to count to 180 (3 Minutes)
+//
+//
+ for index in 1...180
+{
+    print("\(index)Egg is not prepared")
+    if (index == 180){
+        print ("Egg is now ready!")
+        
+    }
+}
+
+
+
